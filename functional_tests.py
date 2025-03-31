32211020b4d1a0dbaa058f35a546977fa63a2a0f
mengmomengshang <2454663917@qq.com>
2025-03-31 13:13:09 +0800

    message 11: Show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -12,8 +12,8 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
+        table = self.browser.find_element(By.ID,'id_list_table')
+        rows = table.find_elements(By.TAG_NAME,'tr')
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
@@ -45,7 +45,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # 页面中又显示了一个文本输入框，可以输入其他待办事项
         # 他输入了"Give a gift to Lisi"
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
         inputbox.send_keys('Give a gift to Lisi')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
