2c67d863d586e65c9a252430a424ed8cb0eddee6
mengmomengshang <2454663917@qq.com>
2025-04-28 13:10:28 +0800

    message 18: new URL and view for adding to existing lists. UT and FT pass

--- lists/views.py
+++ lists/views.py
@@ -3,19 +3,19 @@ from django.http import HttpResponse
 from lists.models import Item, List
 
 def home_page(request):
-    #if request.method == 'POST':
-    #    Item.objects.create(text = request.POST['item_text'])
-    #    return redirect('/lists/the-new-page/')
     return render(request, 'home.html')
 
-    #items = Item.objects.all()
-    #return render(request, 'home.html', {'items' : items})
 # Create your views here.
-def view_list(request):
-    items = Item.objects.all()
-    return render(request, 'list.html', {'items' : items})
+def view_list(request, list_id):
+    list_user = List.objects.get(id=list_id)
+    return render(request, 'list.html', {'list' : list_user})
 
 def new_list(request):
     list_user = List.objects.create()
     Item.objects.create(text = request.POST['item_text'], list=list_user)
-    return redirect('/lists/the-new-page/')
+    return redirect(f'/lists/{list_user.id}/')
+
+def add_item(request, list_id):
+    list_user = List.objects.get(id=list_id)
+    Item.objects.create(text = request.POST['item_text'], list=list_user)
+    return redirect(f'/lists/{list_user.id}/')
